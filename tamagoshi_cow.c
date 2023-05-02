74228e24052e323e3b233d5dcee060faa66f171b
lukerabbitte <luke.rabbitte@gmail.com>
2023-05-02 13:22:12 +0100

    Error checking for input

--- tamagoshi_cow.c
+++ tamagoshi_cow.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <ctype.h>
 
-const int vide = 0;
-const int saturee = 10;
-const int niveau_fitness_initiale = 5;
-const int niveau_stock_initiale = 5;
+const int min_stock = 0;
+const int max_stock = 10;
+
+int fitness = 5;
+int stock = 5;
 
 void affiche_vache(int fitness) {
 
@@ -28,7 +31,7 @@ void affiche_vache(int fitness) {
         case 8:
         case 9:
             printf("        \\   ^__^         /\n");
-            printf("         \\  (XX)\\_______/\n");
+            printf("         \\  (..)\\_______/\n");
             printf("            <__>\\-----w-)     \n");
             printf("              U |      |\n");
             printf("                 |     |\n");
@@ -55,62 +58,85 @@ void affiche_vache(int fitness) {
             break;
 
         default:
-            printf("Erreur : entrée inattendue. La condition physique des vaches doit être comprise entre 0 et 10 inclus.\n");
+            printf("Erreur : entree inattendue. La condition physique des vaches doit etre comprise entre 0 et 10 inclus.\n");
             break;
     }
 }
 
+int fitness_update(int lunchfood) {
+    int digestion = rand() % 10;
+    return (fitness + lunchfood) + digestion;
+}
 
+int stock_update(int lunchfood) {
+    int crop = rand() % 20;
+    return (stock - lunchfood) + crop;
+}
 
-int main(int argc, char* argv[]) {
+int main() {
+
+    srand(time(NULL)); // initialiser le germe aléatoire
 
     // On déclare les variables qui influencent le jeu.
-    int fitness = niveau_fitness_initiale;
-    int stock = niveau_stock_initiale;
     int digestion; // sera généré aléatoirement sur chaque boucle entre 0 et 3 inclus
     int crop; // sera généré aléatoirement sur chaque boucle entre -3 et 3 inclus.
     int lunchfood; // sera demandé à chaque fois à l'utilisateur. N'acceptez les entrées de l'utilisateur que si <=stock
-    int duree_de_vie; // iterateur dans la boucle principale
+    int lifespan = 0; // iterateur dans la boucle principale
+    int validInput = 0; // la gestion des erreurs
 
-    // Start timer
+    int duree_de_vie = 0;
+    int time_elapsed = 0;
+    clock_t start_time = clock(); // Démarrer la minuterie
 
-    /* Enter loop
-     * - Print cow according to current fitness level
-     * - Print current stock
-     * - Ask user what value of lunchfood they want to give to the cow, checking it is greater than stock.
-     * - Update fitness
-     *      fitness_update(int lunchfood)
-     *      - can access fitness
-     *      - generate digestion
-     *      - return (fitness + lunchfood) + digestion
-     *
-     * - If fitness between 1 and 9 inclusive,
-     * - Then increment duree_de_vie
-     * - Then update stock to add or subtract randomly generated crop and proceed with the loop.
-     *      This is achieved with stock_update(int lunchfood) within which we
-     *      - can access stock, even though we're in a new function
-     *      - generate crop
-     *      - return (stock-lunchfood) + crop
-     * - Else break out of the loop - we have lost the game
-     */
+    while (1) {
 
-    // End timer
+        // Afficher la vache et le stock
+        affiche_vache(fitness);
+        printf("\nStock actuel: %d\n", stock);
 
-    /*
-     * - Output final score (time elapsed in seconds).
-     * - Output ASCII trophy based on how well the user did. Give bronze, silver, and gold medals.
-     */
+        // Demander à l'utilisateur la valeur de lunchfood
+        while (!validInput) {
+            printf("Donner la valeur de lunchfood pour la vache:");
+            scanf("%d", &lunchfood);
 
-    /*
-     * Question: is the user allowed to input a lunchfood that, combined with digestion,
-     * will produce a fitness way over 10? In this case, do we move to overate stage? Or do we circle
-     * back and ask the user for another lunchfood value?
-    */
+            if (scanf("%d", &lunchfood) != 1 || getchar() != '\n' || !isdigit(lunchfood)) {
+                printf("Error: Please enter a valid integer value.\n");
+                fflush(stdin);
+            }
+            else if (lunchfood > stock) {
+                printf("La valeur de Lunchfood depasse le stock actuel. Essayer a nouveau.\n");
+            }
+            else if (lunchfood <= stock) {
+                validInput=1;
+            }
+            else
+                printf("Erreur: entrez un nombre entier inferieur ou egal au niveau de stock actuel");
+        }
 
+        // Mettre à jour la condition physique et le stock
+        fitness = fitness_update(lunchfood);
+        if (fitness >= 1 && fitness <= 9) {
+            duree_de_vie++;
+            stock = stock_update(lunchfood);
+        } else {
+            break;
+        }
+    }
 
-    printf("Enter a choice (1, 2, or 3): ");
-    scanf("%d", &choice);
+    // Arrêter la minuterie
+    clock_t end_time = clock();
+    time_elapsed = (end_time - start_time) / CLOCKS_PER_SEC;
+    printf("\nTemps total ecoule: %d secondes\n", time_elapsed);
+
+    // Afficher le score final et le trophée
+    printf("Score final: %d secondes ecoulees\n", time_elapsed);
+    if (duree_de_vie >= 20) {
+        printf("Medaille d'or obtenue!\n");
+    } else if (duree_de_vie >= 10) {
+        printf("Medaille d'argent obtenue!\n");
+    } else {
+        printf("Medaille de bronze obtenue!\n");
+    }
 
     return 0;
 }
-
